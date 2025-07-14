7270009aed43481cb234dc10082dca19029ab864
rafas <rafasyudi@gmail.com>
2025-07-14 16:26:21 -0300

    Ta funfando o codigo do Ryan

--- src/main/java/org/example/MyLanguageSemanticAnalyzer.java
+++ src/main/java/org/example/MyLanguageSemanticAnalyzer.java
@@ -1,87 +1,76 @@
 package org.example;
 
-// Import the generated base listener
-// Ensure your ANTLR output package is correct if it's not org.example
-// e.g. import com.yourlanguage.parser.ObjectOrientedParserBaseListener;
-// import com.yourlanguage.parser.ObjectOrientedParser;
+// Import the generated base listener and parser classes
+import org.example.ObjectOrientedParserBaseListener;
+import org.example.ObjectOrientedParser;
 
-public class MyLanguageSemanticAnalyzer extends org.example.ObjectOrientedParserBaseListener {
+public class MyLanguageSemanticAnalyzer extends ObjectOrientedParserBaseListener {
 
     @Override
-    public void enterClassDeclaration(org.example.ObjectOrientedParser.ClassDeclarationContext ctx) {
-        // ctx.IDENTIFIER() gives you the token for the class name
+    public void enterClassDeclaration(ObjectOrientedParser.ClassDeclarationContext ctx) {
         String className = ctx.IDENTIFIER().getText();
         System.out.println("Entering class: " + className);
-        // Here you would typically:
         // 1. Create a new scope for this class in your symbol table.
-        // 2. Add the class itself to the current (e.g., package) scope.
+        // 2. Add the class itself to the current scope.
     }
 
     @Override
-    public void exitClassDeclaration(org.example.ObjectOrientedParser.ClassDeclarationContext ctx) {
+    public void exitClassDeclaration(ObjectOrientedParser.ClassDeclarationContext ctx) {
         String className = ctx.IDENTIFIER().getText();
         System.out.println("Exiting class: " + className);
-        // Here you would typically:
         // 1. Pop the class scope from your symbol table stack.
     }
 
     @Override
-    public void enterMethodDeclaration(org.example.ObjectOrientedParser.MethodDeclarationContext ctx) {
-        // ctx.IDENTIFIER() gives you the token for the method name
-        String methodName = ctx.IDENTIFIER().getText();
-        System.out.println("  Entering method: " + methodName);
+    public void enterFieldDeclaration(ObjectOrientedParser.FieldDeclarationContext ctx) {
+        String type = ctx.type().getText();
 
-        // You can also access other parts, e.g., return type
-        // ObjectOrientedParser.TypeOrVoidContext returnTypeCtx = ctx.typeOrVoid();
-        // String returnType = returnTypeCtx.getText();
-        // System.out.println("    Return type: " + returnType);
+        // Loop through all variables declared on the same line
+        for (ObjectOrientedParser.VariableDeclaratorContext varCtx : ctx.variableDeclaratorList().variableDeclarator()) {
+            String varName = varCtx.IDENTIFIER().getText();
+            System.out.println("  Found field: " + type + " " + varName);
 
-        // Access parameters:
-        // ObjectOrientedParser.FormalParameterListContext paramsCtx = ctx.formalParameterList();
-        // if (paramsCtx != null) {
-        //     System.out.println("    Parameters:");
-        //     for (ObjectOrientedParser.FormalParameterContext paramCtx : paramsCtx.formalParameter()) {
-        //         String paramName = paramCtx.variableDeclaratorId().IDENTIFIER().getText();
-        //         String paramType = paramCtx.type().getText();
-        //         System.out.println("      - " + paramType + " " + paramName);
-        //     }
-        // }
-
-        // Here you would typically:
-        // 1. Create a new scope for this method.
-        // 2. Add method parameters to this new scope.
-        // 3. Add the method to the current class's symbol table.
-    }
-
-    @Override
-    public void exitMethodDeclaration(org.example.ObjectOrientedParser.MethodDeclarationContext ctx) {
-        String methodName = ctx.IDENTIFIER().getText();
-        System.out.println("  Exiting method: " + methodName);
-        // Here you would typically:
-        // 1. Pop the method scope.
-        // 2. Perform type checking for the method body (if not done incrementally).
-    }
-
-    // You would override other methods like enterFieldDeclaration, enterVariableDeclarator,
-    // enterExpression, etc., to build your symbol table, perform type checking, etc.
-    // For example, to check an assignment:
-    /*
-    @Override
-    public void exitAssignmentExpression(ObjectOrientedParser.AssignmentExpressionContext ctx) {
-        if (ctx.assignmentOperator() != null) { // It's an actual assignment like x = y, not just an expression
-            // Get the type of the left-hand side (LHS)
-            // ObjectOrientedParser.ConditionalExpressionContext lhsCtx = ctx.conditionalExpression(0);
-            // MyType lhsType = evaluateTypeOf(lhsCtx); // You'll need a way to get types
-
-            // Get the type of the right-hand side (RHS)
-            // ObjectOrientedParser.ConditionalExpressionContext rhsCtx = ctx.conditionalExpression(1);
-            // MyType rhsType = evaluateTypeOf(rhsCtx);
-
-            // System.out.println("Checking assignment: " + lhsCtx.getText() + " " + ctx.assignmentOperator().getText() + " " + rhsCtx.getText());
-            // if (!isAssignable(lhsType, rhsType)) {
-            //    System.err.println("Type error: Cannot assign " + rhsType + " to " + lhsType + " at line " + ctx.start.getLine());
-            // }
+            // Check for an initializer
+            if (varCtx.variableInitializer() != null) {
+                String value = varCtx.variableInitializer().getText();
+                System.out.println("    - with initial value: " + value);
+            }
         }
     }
-    */
+
+    @Override
+    public void enterMethodDeclaration(ObjectOrientedParser.MethodDeclarationContext ctx) {
+        String methodName = ctx.IDENTIFIER().getText();
+        System.out.println("  Entering method: " + methodName);
+        // 1. Create a new scope for this method.
+        // 2. Add method parameters to this new scope.
+    }
+
+    @Override
+    public void exitMethodDeclaration(ObjectOrientedParser.MethodDeclarationContext ctx) {
+        String methodName = ctx.IDENTIFIER().getText();
+        System.out.println("  Exiting method: " + methodName);
+        // 1. Pop the method scope.
+    }
+
+    /**
+     * Listener for an addition expression. This is triggered when the walker
+     * exits an additiveExpression rule in the parser that involves an ADD operator.
+     */
+    @Override
+    public void exitAdditiveExpression(ObjectOrientedParser.AdditiveExpressionContext ctx) {
+        // Check if this expression is an addition or subtraction
+        if (ctx.ADD() != null) {
+            // This rule is structured as 'multiplicativeExpression (ADD multiplicativeExpression)*'
+            // We can get the left and right operands like this:
+            String leftOperand = ctx.multiplicativeExpression(0).getText();
+            String rightOperand = ctx.multiplicativeExpression(1).getText();
+
+            System.out.println("  Detected addition: " + leftOperand + " + " + rightOperand);
+
+            // In a real compiler, you would perform type checking here
+            // to ensure both operands are numeric.
+        }
+        // You could add a similar check for subtraction (ctx.SUB() != null)
+    }
 }
